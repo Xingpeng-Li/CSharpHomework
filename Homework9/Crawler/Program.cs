c251e9b8c5d829e9af5a5803a6320b71d26182b0
LXP <1944256523@qq.com>
2020-04-18 00:48:27 +0800

    第九次作业

---
+++ Homework9/Crawler/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Crawler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String startUrl = "http://www.cnblogs.com/dstang2000/";
+            if (args.Length >= 1) startUrl = args[0];
+            Crawler myCrawler = new Crawler(startUrl);
+            new Thread(myCrawler.Crawl).Start();
+        }
+    }
+}
